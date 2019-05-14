fd3f6fb4d97f6162c9ac43502631bf4732790664
SomeTake <tp1002217g@gmail.com>
2019-05-14 19:49:37 +0900

    Map
    csv読み込み追加

--- BrushRunner/SceneCharacterSelect.h
+++ BrushRunner/SceneCharacterSelect.h
@@ -1,6 +1,15 @@
+//=============================================================================
+//
+// キャラクターセレクト画面処理 [SceneCharacterSelect.h]
+// Author : HAL東京 GP12B332-19 80277 染谷武志
+//
+//=============================================================================
 #ifndef _SCENECHARACTERSELECT_H_
 #define _SCENECHARACTERSELECT_H_
 
+//*****************************************************************************
+// プロトタイプ宣言
+//*****************************************************************************
 HRESULT InitSceneCharacterSelect();
 void UninitSceneCharacterSelect();
 void UpdateSceneCharacterSelect();
