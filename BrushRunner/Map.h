76accb28da05d1d1452236a19396cab9a3141ccb
SomeTake <tp1002217g@gmail.com>
2019-05-16 15:12:52 +0900

    MapUpdate

--- BrushRunner/Map.h
+++ BrushRunner/Map.h
@@ -17,10 +17,11 @@
 #define MAP_TEXTURE0	("data/TEXTURE/map0.jpg")
 #define MAP_TEXTURE1	("data/TEXTURE/map1.jpg")
 #define MAP_TEXTURE2	("data/TEXTURE/map2.jpg")
-#define MAP_FILE		("data.csv")				// 読み込むマップデータ
-#define MAP_SIZE_X		(10)						// マップの横の枚数
-#define MAP_SIZE_Y		(10)						// マップの縦の枚数
-#define CHIP_SIZE		(40.0f)						// マップチップ一枚のサイズ
+#define MAP_POS			D3DXVECTOR3(0.0f, 0.0f, 0.0f)	// 表示場所
+#define MAP_FILE		("data/MAP/data.csv")			// 読み込むマップデータ
+#define MAP_SIZE_X		(3)							// マップの横の枚数
+#define MAP_SIZE_Y		(3)								// マップの縦の枚数
+#define CHIP_SIZE		(40.0f)							// マップチップ一枚のサイズ
 
 // マップチップの種類
 enum {
@@ -28,7 +29,7 @@ enum {
 	MapTrap,
 	MapItem,
 
-	// マップチップの数
+	// マップチップの種類数
 	MapChipMax
 };
 
@@ -38,14 +39,14 @@ enum {
 class MAP
 {
 private:
-	string					line;							// 文字列を一時的に保存
-	int						maptbl[MAP_SIZE_Y][MAP_SIZE_X];	// マップ用の配列
-	const string			delim = ",";					// データ区切り用の文字
+	string					line;								// 文字列を一時的に保存
+	int						maptbl[MAP_SIZE_Y][MAP_SIZE_X];		// マップ用の配列
+	const string			delim = ",";						// データ区切り用の文字
 	D3DXVECTOR3				pos;
 	D3DXVECTOR3				rot;
 	D3DXVECTOR3				scl;
-	LPDIRECT3DTEXTURE9		D3DTexture = NULL;				// テクスチャへのポインタ
-	LPDIRECT3DVERTEXBUFFER9 D3DVtxBuff = NULL;				// 頂点バッファへのポインタ
+	LPDIRECT3DTEXTURE9		D3DTexture[MapChipMax] = { NULL };	// テクスチャへのポインタ
+	LPDIRECT3DVERTEXBUFFER9 D3DVtxBuff = NULL;					// 頂点バッファへのポインタ
 
 
 public:
