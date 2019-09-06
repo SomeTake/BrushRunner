e307015c60863839e4809ec184e886ab289b27d0
SomeTake <tp1002217g@gmail.com>
2019-09-06 13:44:48 +0900

    KeyDebug
    ジャンプを上キーからUキーに変更

--- BrushRunner/IdleState.cpp
+++ BrushRunner/IdleState.cpp
@@ -43,7 +43,7 @@ void IdleState::Update(int AnimCurtID)
 		else
 		{
 			// 上キーを押してジャンプ
-			if (GetKeyboardTrigger(DIK_UP) || IsButtonTriggered(owner_->GetCtrlNum(), BUTTON_B) 
+			if (GetKeyboardTrigger(DIK_U) || IsButtonTriggered(owner_->GetCtrlNum(), BUTTON_B) 
 				|| owner_->GetAIAction() == eActJump)
 			{
 				float value = owner_->GetJumpValue();
--- BrushRunner/SceneGame.cpp
+++ BrushRunner/SceneGame.cpp
@@ -64,12 +64,12 @@ SceneGame::SceneGame()
 	//}
 
 #if _DEBUG
-	pPlayer[0] = new Player(0, true);
-	pPlayer[1] = new Player(1, true);
+	pPlayer[0] = new Player(0, false);
+	pPlayer[1] = new Player(1, false);
 	//pPlayer[1]->SetOnCamera(false);
-	pPlayer[2] = new Player(2, true);
+	pPlayer[2] = new Player(2, false);
 	//pPlayer[2]->SetOnCamera(false);
-	pPlayer[3] = new Player(3, true);
+	pPlayer[3] = new Player(3, false);
 	//pPlayer[3]->SetOnCamera(false);
 #endif
 
